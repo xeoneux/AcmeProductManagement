7603fb72e0727f898e21e9b1350a3ad17570b839
Aayush Kapoor <xeoneux@icloud.com>
2016-04-15 14:55:08 +0530

    Use product list component as directive

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from 'angular2/core';
 
+import { ProductListComponent } from './products/product-list.component';
+
 @Component({
   selector: 'pm-app',
   template: `
   <div>
     <h1>{{pageTitle}}</h1>
-    <div>My First Component</div>
+    <pm-products></pm-products>
   </div>
-  `
+  `,
+  directives: [ProductListComponent]
 })
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';

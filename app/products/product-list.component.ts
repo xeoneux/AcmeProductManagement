11fb9905c833f196752d3c63264adac645163a78
Aayush Kapoor <xeoneux@icloud.com>
2016-04-15 21:03:19 +0530

    Toggle show or hide image

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -8,6 +8,7 @@ export class ProductListComponent {
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
+  showImage: boolean = false;
   products: any[] = [
     {
       "productId": 1,
@@ -60,4 +61,8 @@ export class ProductListComponent {
       "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
     }
   ];
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 }

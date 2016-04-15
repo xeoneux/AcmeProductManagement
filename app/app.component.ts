3e99b6b920ddc241f01fafef7c24a7f4e96c40b7
Aayush Kapoor <xeoneux@icloud.com>
2016-04-15 14:35:09 +0530

    Create app component

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'pm-app',
+  template: `
+  <div>
+    <h1>{{pageTitle}}</h1>
+    <div>My First Component</div>
+  </div>
+  `
+})
+export class AppComponent {
+  pageTitle: string = 'Acme Product Management';
+}

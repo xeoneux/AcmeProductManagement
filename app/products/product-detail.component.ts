21cfc8c58c5b5c22141b4391fe2b93ddbccf9a53
Aayush Kapoor <xeoneux@icloud.com>
2016-04-22 11:47:06 +0530

    Get and store route parameter

--- app/products/product-detail.component.ts
+++ app/products/product-detail.component.ts
@@ -1,8 +1,14 @@
 import { Component } from 'angular2/core';
+import { RouteParams } from 'angular2/router';
 
 @Component({
-  templateUrl: 'app/product/product-detail.component.html'
+  templateUrl: 'app/products/product-detail.component.html'
 })
 export class ProductDetailComponent {
   pageTitle: string = 'Product Detail';
+
+  constructor(private _routerParams: RouteParams) {
+    let id = +this._routerParams.get('id');
+    this.pageTitle += `: ${id}`;
+  }
 }

34f262cdab6093699d2532d2d8d3947c3b1bfc48
Aayush Kapoor <xeoneux@icloud.com>
2016-04-22 10:30:22 +0530

    Handle HTTP exception

--- app/products/product.service.ts
+++ app/products/product.service.ts
@@ -15,5 +15,12 @@ export class ProductService {
   getProducts(): Observable<IProduct[]> {
     return this._http.get(this._productUrl)
       .map((response: Response) => <IProduct[]>response.json())
+      .do(data => console.log("All: " + JSON.stringify(data)))
+      .catch(this.handleError);
+  }
+
+  private handleError(error: Response) {
+    console.error(error);
+    return Observable.throw(error.json().error || 'Server error');
   }
 }

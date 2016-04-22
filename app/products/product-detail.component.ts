40ebf891f67b284b73b31fb48312a2c034c9ef68
Aayush Kapoor <xeoneux@icloud.com>
2016-04-22 11:16:32 +0530

    Add product detail component

---
+++ app/products/product-detail.component.ts
@@ -0,0 +1,8 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  templateUrl: 'app/product/product-detail.component.html'
+})
+export class ProductDetailComponent {
+  pageTitle: string = 'Product Detail';
+}

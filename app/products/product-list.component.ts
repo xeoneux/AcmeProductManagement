460a1cb6ca01304f7fde158adc2bb50f8f68dc6f
Aayush Kapoor <xeoneux@icloud.com>
2016-04-22 11:39:10 +0530

    Add router outlet

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -6,7 +6,6 @@ import { ProductService } from './product.service';
 import { StarComponent } from '../shared/star.component';
 
 @Component({
-  selector: 'pm-products',
   templateUrl: 'app/products/product-list.component.html',
   styleUrls: ['app/products/product-list.component.css'],
   pipes: [ProductFilterPipe],
